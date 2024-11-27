bcbc225bfa87c5098a2d80c788c1266a90291e8e
Tami Rodriguez <113520005+TamiJessica-Rodriguez@users.noreply.github.com>
2024-11-27 12:34:11 +0100

    connected database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,17 @@
-import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import { app } from './app';
-dotenv.config();
+// dotenv.config();
 
 // const dotenv = require('dotenv');
 // dotenv.config();
 
-// const mongoose = require('mongoose');
-
 // HÄR SKRIVER NI KODEN FÖR ATT ANSLUTA TILL DATABASEN OCH STARTA SERVERN!
-main().catch((error) => console.error(error));
+// main().catch((error) => console.error(error));
 
 async function main() {
     // console.log(process.env.MONGO_URI!);
     await mongoose.connect('mongodb+srv://tamijessicarodriguez:Z4eC95Ccrgf7UFD5@flashmath.hb7x4.mongodb.net/?retryWrites=true&w=majority&appName=FlashMath'), console.log('connected to database');
     app.listen(3000, () => console.log('hello world', 'http://localhost:3000'));
 }
+
+main().catch((error) => console.error(error));
